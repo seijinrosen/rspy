1bdb30c8f0bd750c99f2a00a23b43ad2f5928253
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-09 07:29:29 +0900

    input() のリーダー部分を分割して、テストを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
 pub fn input(prompt: &str) -> String {
     print!("{}", prompt);
     io::stdout().flush().unwrap();
+
+    input_inner(io::stdin().lock())
+}
+
+fn input_inner(mut reader: impl BufRead) -> String {
     let mut buf = String::new();
-    io::stdin().read_line(&mut buf).unwrap();
+    reader.read_line(&mut buf).unwrap();
     buf.pop();
     buf
 }
@@ -21,5 +26,14 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn input_inner_works() {
+        // https://stackoverflow.com/a/72187752
+        // https://stackoverflow.com/a/28370712
+        let user_input = b"I'm George\n";
+        let result = input_inner(&user_input[..]);
+        assert_eq!(result, "I'm George");
     }
 }
