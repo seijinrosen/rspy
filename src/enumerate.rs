b1af0d8a09086d7edc43d2d3e97d4007a7da8430
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-13 01:33:11 +0900

    Add doc and test

--- src/enumerate.rs
+++ src/enumerate.rs
@@ -24,12 +24,55 @@ where
     }
 }
 
+/// `str` を拡張するためのトレイト。
 pub trait PyString {
+    /// インデックス付きでイテレートします。
+    ///
+    /// Python とは異なり、`start` は 0 の場合でも必須です。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rspy::PyString;
+    ///
+    /// let mut index_vec = vec![];
+    /// let mut char_vec = vec![];
+    ///
+    /// for (i, ch) in "abcde".enumerate(-3) {
+    ///     index_vec.push(i);
+    ///     char_vec.push(ch);
+    /// }
+    ///
+    /// assert_eq!(index_vec, [-3, -2, -1, 0, 1]);
+    /// assert_eq!(char_vec, ['a', 'b', 'c', 'd', 'e']);
+    /// ```
     fn enumerate(&self, start: i32) -> PyEnumerate<Chars>;
 }
 
 impl PyString for str {
+    /// インデックス付きでイテレートします。
     fn enumerate(&self, start: i32) -> PyEnumerate<Chars> {
         PyEnumerate::new(self.chars(), start)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn enumerate_works() {
+        let mut index_vec = vec![];
+        let mut char_vec = vec![];
+
+        for (i, ch) in "abcde".enumerate(-3) {
+            index_vec.push(i);
+            char_vec.push(ch);
+        }
+
+        assert_eq!(index_vec, [-3, -2, -1, 0, 1]);
+        assert_eq!(char_vec, ['a', 'b', 'c', 'd', 'e']);
     }
 }
