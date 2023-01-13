25e393bf4103568ac0dbf3fb1dc9349a1371cd39
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-14 05:36:58 +0900

    Add tests: mkdir

--- src/pathlib.rs
+++ src/pathlib.rs
@@ -30,10 +30,11 @@ impl PyPath for Path {
 mod tests {
     use super::*;
 
+    use std::fs::File;
     use tempfile::tempdir;
 
     #[test]
-    fn both_false_ok() -> Result<()> {
+    fn mkdir_flat() -> Result<()> {
         let temp_dir = tempdir()?;
 
         let mock_dir_path = temp_dir.path().join("mock_dir");
@@ -42,7 +43,71 @@ mod tests {
         mock_dir_path.mkdir(false, false)?;
         assert!(mock_dir_path.is_dir());
 
+        let result = mock_dir_path.mkdir(false, false);
+        assert!(result.is_err());
+
+        mock_dir_path.mkdir(false, true)?;
+
         drop(mock_dir_path);
+        temp_dir.close()?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn mkdir_nested() -> Result<()> {
+        let temp_dir = tempdir()?;
+
+        let nested_mock_dir_path = temp_dir.path().join("mock_dir").join("mock_dir2");
+        assert!(!nested_mock_dir_path.exists());
+
+        let result = nested_mock_dir_path.mkdir(false, false);
+        assert!(result.is_err());
+
+        let result = nested_mock_dir_path.mkdir(false, true);
+        assert!(result.is_err());
+
+        assert!(!nested_mock_dir_path.exists());
+
+        nested_mock_dir_path.mkdir(true, false)?;
+        assert!(nested_mock_dir_path.is_dir());
+
+        let result = nested_mock_dir_path.mkdir(false, false);
+        assert!(result.is_err());
+
+        let result = nested_mock_dir_path.mkdir(true, false);
+        assert!(result.is_err());
+
+        nested_mock_dir_path.mkdir(false, true)?;
+        nested_mock_dir_path.mkdir(true, true)?;
+
+        drop(nested_mock_dir_path);
+        temp_dir.close()?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn mkdir_file_exists() -> Result<()> {
+        let temp_dir = tempdir()?;
+
+        let file_path = temp_dir.path().join("mock_file");
+        let file = File::create(&file_path)?;
+        assert!(file_path.is_file());
+
+        let result = file_path.mkdir(false, false);
+        assert!(result.is_err());
+
+        let result = file_path.mkdir(false, true);
+        assert!(result.is_err());
+
+        let result = file_path.mkdir(true, false);
+        assert!(result.is_err());
+
+        let result = file_path.mkdir(true, true);
+        assert!(result.is_err());
+
+        drop(file);
         temp_dir.close()?;
 
         Ok(())
