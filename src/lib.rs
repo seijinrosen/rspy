6caf46df98737e61e3abe61bcd00dbdceef58df3
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-15 07:45:58 +0900

    Rename mod name: enumerate -> iterable

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 //! Pythonic interface for Rust.
 
-mod enumerate;
 mod input;
+mod iterable;
 pub mod pathlib;
 pub mod string;
 pub mod time;
 
-pub use crate::enumerate::enumerate;
-pub use crate::enumerate::Iterable;
 pub use crate::input::input;
+pub use crate::iterable::enumerate;
+pub use crate::iterable::Iterable;
