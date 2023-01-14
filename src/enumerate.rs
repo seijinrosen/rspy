e7e48d5647cdaa43d71b51a08970dbf0edf4cc75
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-15 05:42:18 +0900

    Add doc, test: enumerate

--- src/enumerate.rs
+++ src/enumerate.rs
@@ -1,5 +1,29 @@
 use std::{iter::Zip, ops::Range, slice::Iter, str::Chars};
 
+/// インデックス付きでイテレートします。
+///
+/// Python とは異なり、`start` は 0 の場合でも必須です。
+///
+/// # Examples
+///
+/// ```
+/// use rspy::enumerate;
+///
+/// let mut index_vec = vec![];
+/// let mut char_vec = vec![];
+///
+/// let s = "abcde";
+///
+/// for (i, ch) in enumerate(&s, -10) {
+///     index_vec.push(i);
+///     char_vec.push(ch);
+/// }
+///
+/// assert_eq!(index_vec, [-10, -9, -8, -7, -6]);
+/// for (i, ch) in s.chars().enumerate() {
+///     assert_eq!(char_vec[i], ch);
+/// }
+/// ```
 pub fn enumerate<'a, T>(iterable: &'a T, start: i32) -> Zip<Range<i32>, T::Item>
 where
     T: Iterable<'a>,
@@ -226,6 +250,24 @@ mod tests {
         assert_eq!(index_vec, [100, 101, 102, 103, 104]);
         for (i, v) in arr.iter().enumerate() {
             assert_eq!(int_vec[i], v);
+        }
+    }
+
+    #[test]
+    fn bare_enumerate_str() {
+        let mut index_vec = vec![];
+        let mut char_vec = vec![];
+
+        let s = "abcde";
+
+        for (i, ch) in enumerate(&s, -10) {
+            index_vec.push(i);
+            char_vec.push(ch);
+        }
+
+        assert_eq!(index_vec, [-10, -9, -8, -7, -6]);
+        for (i, ch) in s.chars().enumerate() {
+            assert_eq!(char_vec[i], ch);
         }
     }
 }
